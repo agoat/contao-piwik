89b77e2434328bf199c2768f57757b9815756244
Arne Stappen <meh@agoat.de>
2016-04-28 17:47:36 +0200

    Adds now always a setDocumentTitle to use either title or pageTitle

--- system/modules/piwikanalytics/classes/Piwik.php
+++ system/modules/piwikanalytics/classes/Piwik.php
@@ -35,15 +35,27 @@ class Piwik extends Frontend
 			
 				$jsTag  = '<script type="text/javascript">' . "\n";
 				$jsTag .= 'var _paq = _paq || []; ' . "\n";
+				
 				// 404 errors
 				if($siteDetails->piwik404 AND $objPage->type == 'error_404')
 				{
 					$jsTag .= ' _paq.push(["setDocumentTitle", "404/URL = " + encodeURIComponent(document.location.pathname+document.location.search) + "/From = " + encodeURIComponent(document.referrer) + "]);' . "\n";
 				}
-				// use page title
-				elseif($siteDetails->piwikPageTitle)
+				
+				// set document title
+				else
 				{
-					$title = $objPage->pageTitle ? $objPage->pageTitle : $objPage->title;
+					// use page title
+					if($siteDetails->piwikPageTitle)
+					{
+						$title = $objPage->pageTitle ? $objPage->pageTitle : $objPage->title;
+					}
+					else
+					{
+						$title = $objPage->title;
+
+					}
+					
 					// add page structure
 					if($siteDetails->piwikAddSiteStructure)
 					{
@@ -61,21 +73,25 @@ class Piwik extends Frontend
 					$title = $siteDetails->piwikAddDomain ? $domain . '/' . $pretitle . $title : $pretitle . $title;
 					$jsTag .= ' _paq.push(["setDocumentTitle", "' . $title . '"]);' . "\n";
 				}
+				
 				// notice if the user do not wish to be tracked
 				if($siteDetails->piwikDoNotTrack) 
 				{
 					$jsTag .= ' _paq.push(["setDoNotTrack", true]);' . "\n";
 				}
+				
 				// track user over subdomains
 				if($siteDetails->piwikCookieDomains) 
 				{
 					$jsTag .= ' _paq.push(["setCookieDomain", "*.' . $domain . '"]);' . "\n";
 				}
+				
 				// set all subdomains to track as local
 				if($siteDetails->piwikSubdomains) 
 				{
 					$jsTag .= ' _paq.push(["setDomains", "*.' . $domain . '"]);' . "\n";
 				}
+				
 				// set specific domains and Files&Assets URL as local
 				elseif(TL_FILES_URL || $objPage->staticSystem || $objPage->staticPlugins || $siteDetails->piwikDomains) 
 				{
@@ -86,11 +102,13 @@ class Piwik extends Frontend
 					$domains = array_unique($domains);
 					$jsTag .= ' _paq.push(["setDomains", ["' . implode("\",\"",$domains) . '"]]);' . "\n";
 				}
+				
 				// set user language
 				if($siteDetails->piwikCustVarLanguage) 
 				{
 					$jsTag .= ' _paq.push(["setCustomVariable", 1, "Language", "' . $objPage->language . '", "visit"]);' . "\n";
 				}
+				
 				// set user logged in status
 				if($siteDetails->piwikCustVarUserName) 
 				{
@@ -98,21 +116,25 @@ class Piwik extends Frontend
 					$userstatus = (FE_USER_LOGGED_IN) ? $this->User->firstname . ' ' . $this->User->lastname . ' (' . $this->User->username . ')' : 'Anonymous';
 					$jsTag .= ' _paq.push(["setCustomVariable", 2, "User", "' . $userstatus . '", "visit"]);' . "\n";
 				}
+				
 				// set custom variable for visit 
 				if($siteDetails->piwikCustVarVisitName && $siteDetails->piwikCustVarVisitValue) 
 				{
 					$jsTag .= ' _paq.push(["setCustomVariable", 3, "' . $siteDetails->piwikCustVarVisitName . '", "' . $siteDetails->piwikCustVarVisitValue . '", "visit"]);' . "\n";
 				}
+				
 				// set page category
 				if($siteDetails->piwikCustVarUseCat && $pageDetails->piwikCatEnabled && $pageDetails->piwikCustVarCatValue) 
 				{
 					$jsTag .= ' _paq.push(["setCustomVariable", 1, "Category", "' . $pageDetails->piwikCustVarCatValue . '", "page"]);' . "\n";
 				}
+				
 				// set custom variable for page
 				if($siteDetails->piwikCustVarUseCat && $pageDetails->piwikCatEnabled && $pageDetails->piwikCustVarPageName && $pageDetails->piwikCustVarPageValue) 
 				{
 					$jsTag .= ' _paq.push(["setCustomVariable", 2, "' .$pageDetails->piwikCustVarPageName . '", "' . $pageDetails->piwikCustVarPageValue . '", "page"]);' . "\n";
 				}
+				
 				// set download extensions (if not default)
 				if($extensions != '7z,aac,arc,arj,asf,asx,avi,bin,bz,bz2,csv,deb,dmg,doc,exe,flv,gif,gz,gzip,hqx,jar,jpg,jpeg,js,mp2,mp3,mp4,mpg,mpeg,mov,movie,msi,msp,odb,odf,odg,odp,ods,odt,ogg,ogv,pdf,phps,png,ppt,qt,qtm,ra,ram,rar,rpm,sea,sit,tar,tbz,tbz2,tgz,torrent,txt,wav,wma,wmv,wpd,xls,xml,z,zip')
 				{
@@ -153,7 +175,7 @@ class Piwik extends Frontend
 	}
 
 	
-		public function validatePath($strRegexp, $varValue, Widget $objWidget)
+	public function validatePath($strRegexp, $varValue, Widget $objWidget)
 	{
 		if($strRegexp == 'piwikPath')
 		{
